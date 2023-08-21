1c0a437795a9c77b40a556acb7ea703f01836974
Kai Ma <107938829+KaiMa-endeavour@users.noreply.github.com>
2023-08-21 20:56:33 +0800

    Add files via upload

---
+++ Rscripts/commDispersal.R
@@ -0,0 +1,33 @@
+# @Project: DOM-in-MicroEco
+# @File: commDispersal.R
+# @Author: Kai Ma
+# Date: 23/05/2023
+
+
+commDispersal <- function(comm, d, nworker = 4) {
+  # A community of metacommunities: exploring patterns in species distributions across large geographical areas
+  comm[comm > 0] <- 1
+  d <- d[match(rownames(comm), rownames(d)), match(rownames(comm), colnames(d))]
+  xx <- matrix(NA, nrow(comm), nrow(comm))
+  
+  for (i in 1:(nrow(comm) - 1)) {
+    # i=1
+    cat(paste0("Start calculating dispersal (", i, "/", nrow(comm) - 1, ")    ", date(), " ..."), "\n")
+    # second <- rep(0, nrow(comm))
+    cl <- makeCluster(nworker, type = "PSOCK")
+    registerDoParallel(cl)
+    destination <- function(j) {
+      two_sample_spec <- comm[i, ] + comm[j, ]
+      two_sample_spec[two_sample_spec > 0] <- 1
+      m <- sum(two_sample_spec)
+      first <- sum(sapply(1:m, function(k) comm[j, k] * 1 / exp(-log10(d[i, j]))))
+      second <- first / m / (nrow(comm) - 1)
+      second
+    }
+    second <- foreach(j = (i + 1):nrow(comm), .combine = "c") %dopar% destination(j)
+    stopCluster(cl)
+    xx[(i + 1):nrow(comm), i] <- second
+    cat(c(i, "Done.   ", date()), "\n")
+  }
+  stats::as.dist(xx)
+}
