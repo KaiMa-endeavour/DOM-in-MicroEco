5013864f05f087f052bcc68ba07c87aa4dfdbeb5
Kai Ma <107938829+KaiMa-endeavour@users.noreply.github.com>
2023-10-18 20:36:37 +0800

    Update commDispersal.R

--- Rscripts/commDispersal.R
+++ Rscripts/commDispersal.R
@@ -9,21 +9,23 @@ commDispersal <- function(comm, d, nworker = 4) {
   comm[comm > 0] <- 1
   d <- d[match(rownames(comm), rownames(d)), match(rownames(comm), colnames(d))]
   xx <- matrix(NA, nrow(comm), nrow(comm))
-  
+  colnames(xx) <- rownames(comm)
+  rownames(xx) <- rownames(comm)
+
+  destination <- function(j) {
+    two_sample_spec <- comm[i, ] + comm[j, ]
+    two_sample_spec[two_sample_spec > 0] <- 1
+    m <- sum(two_sample_spec)
+    first <- sum(sapply(1:m, function(k) comm[j, k] * 1 / exp(-log10(d[i, j]))))
+    second <- first / m / (nrow(comm) - 1)
+    second
+  }
   for (i in 1:(nrow(comm) - 1)) {
     # i=1
     cat(paste0("Start calculating dispersal (", i, "/", nrow(comm) - 1, ")    ", date(), " ..."), "\n")
     # second <- rep(0, nrow(comm))
     cl <- makeCluster(nworker, type = "PSOCK")
     registerDoParallel(cl)
-    destination <- function(j) {
-      two_sample_spec <- comm[i, ] + comm[j, ]
-      two_sample_spec[two_sample_spec > 0] <- 1
-      m <- sum(two_sample_spec)
-      first <- sum(sapply(1:m, function(k) comm[j, k] * 1 / exp(-log10(d[i, j]))))
-      second <- first / m / (nrow(comm) - 1)
-      second
-    }
     second <- foreach(j = (i + 1):nrow(comm), .combine = "c") %dopar% destination(j)
     stopCluster(cl)
     xx[(i + 1):nrow(comm), i] <- second
