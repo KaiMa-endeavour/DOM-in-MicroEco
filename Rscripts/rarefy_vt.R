39d76d53b6997ebb9f44872e71a78a3935eda47c
Kai Ma <107938829+KaiMa-endeavour@users.noreply.github.com>
2023-12-26 23:31:21 +0800

    Update rarefy_vt.R

--- Rscripts/rarefy_vt.R
+++ Rscripts/rarefy_vt.R
@@ -25,3 +25,20 @@ rarefy_vt <- function(x, depth, prob = NULL, prob_dec = FALSE, replace = FALSE) 
   z[as.numeric(names(y_tab))] <- y_tab
   z
 }
+
+rarefy <- function(otu_table, MARGIN = 1, depth = min(rowSums(otu_table)), prob = NULL, replace = F, nworker = 1) {
+  otu_table <- apply(otu_table, 2, as.numeric)
+  if (nworker == 1) {
+    df <- t(apply(X = otu_table, MARGIN = MARGIN, FUN = rarefy_vt, depth = depth, prob = prob, replace = replace))
+  } else {
+    require(parallel)
+    cl <- makeCluster(nworker, type = "PSOCK")
+    df <- t(parApply(cl, X = otu_table, MARGIN = MARGIN, FUN = rarefy_vt, depth = depth, prob = prob, replace = replace))
+    stopCluster(cl)
+  }
+  rownames(df) <- rownames(otu_table)
+  colnames(df) <- colnames(otu_table)
+  df <- df[rowSums(df) > 0, ]
+  df <- df[, colSums(df) > 0]
+  df <- as.data.frame(df)
+}
