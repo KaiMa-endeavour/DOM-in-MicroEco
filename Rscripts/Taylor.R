70e09c9653630853f5663cdcd1c31a294db6e8c5
Kai Ma <107938829+KaiMa-endeavour@users.noreply.github.com>
2023-12-28 20:07:34 +0800

    Add files via upload

---
+++ Rscripts/Taylor.R
@@ -0,0 +1,29 @@
+# @Project: DOM-in-MicroEco
+# @File: Taylor.R
+# @Author: Kai Ma
+# Date: 13/08/2022
+
+
+library(tidyfst)
+library(dplyr)
+
+data <- fread('./DOM_scaled_rarefied_int_tab.csv')
+proj <- reshape2::melt(data, value.name = 'count', variable.name = 'ASVs') %>% mutate(nreads = rowSums(data[, -1])[1]) %>% filter(count >0) %>% group_by( Samples, ASVs ) %>% mutate( tf = mean(count/nreads), o = n(), tvpf = mean( (count^2 - count)/nreads^2 ) ) %>% ungroup() %>% group_by( Samples ) %>% mutate( f = o*tf, vf = o*tvpf ) #%>% mutate(vf = vf - f^2 )
+gamma_pars_dom <- proj %>% select( Samples, ASVs, o, f, vf ) %>% 
+  mutate( cv = sqrt(vf/f^2) ) %>% distinct() %>% 
+  mutate( beta = 1./cv^2, theta = f/beta ) %>% mutate(group = 'DOM') #%>% sample_frac(size = 0.5)
+
+data <- fread('./ASVstable_rarefied_Bacteria.csv')
+proj <- reshape2::melt(data, value.name = 'count', variable.name = 'ASVs') %>% mutate(nreads = rowSums(data[, -1])[1]) %>% filter(count >0) %>% group_by( Samples, ASVs ) %>% mutate( tf = mean(count/nreads), o = n(), tvpf = mean( (count^2 - count)/nreads^2 ) ) %>% ungroup() %>% group_by( Samples ) %>% mutate( f = o*tf, vf = o*tvpf ) #%>% mutate(vf = vf - f^2 )
+gamma_pars_bac <- proj %>% select( Samples, ASVs, o, f, vf ) %>% 
+  mutate( cv = sqrt(vf/f^2) ) %>% distinct() %>% 
+  mutate( beta = 1./cv^2, theta = f/beta ) %>% mutate(group = 'Bacteria') #%>% sample_frac(size = 0.5)
+
+data <- fread('./ASVstable_rarefied_Fungi.csv')
+proj <- reshape2::melt(data, value.name = 'count', variable.name = 'ASVs') %>% mutate(nreads = rowSums(data[, -1])[1]) %>% filter(count >0) %>% group_by( Samples, ASVs ) %>% mutate( tf = mean(count/nreads), o = n(), tvpf = mean( (count^2 - count)/nreads^2 ) ) %>% ungroup() %>% group_by( Samples ) %>% mutate( f = o*tf, vf = o*tvpf ) #%>% mutate(vf = vf - f^2 )
+gamma_pars_fun <- proj %>% select( Samples, ASVs, o, f, vf ) %>% 
+  mutate( cv = sqrt(vf/f^2) ) %>% distinct() %>% 
+  mutate( beta = 1./cv^2, theta = f/beta ) %>% mutate(group = 'Fungi') #%>% sample_frac(size = 0.5)
+
+
+gamma_pars <- rbind(gamma_pars_dom, gamma_pars_bac, gamma_pars_fun)
